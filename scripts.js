1d2f57e8336baa48769e232184cde71921f894fd
Brandon Cen <bcen96@gmail.com>
2018-01-14 22:34:20 -0800

    basic combat field

--- scripts.js
+++ scripts.js
@@ -1,5 +1,12 @@
-var phases = ["Place Actions", "Flip Actions", "Combat", "End of Combat", "Resolve Combat", "End of Turn", "Upkeep"];
+const phases = ["Place Actions", "Flip Actions", "Combat", "End of Combat", "Resolve Combat", "End of Turn", "Upkeep"];
 var phaseIndex = 0;
+
+// on load, render cards
+window.onload = function() {
+	for (var i = 0; i < 5; i++) {
+		displayCard(i);
+	}
+};
 
 function nextPhase() {
 	phaseIndex++;
@@ -10,4 +17,10 @@ function nextPhase() {
 function displayPhase() {
 	var tag = "Current Phase: ";
 	document.getElementById("phase-label").innerHTML = tag + phases[phaseIndex];
+}
+
+function displayCard(i) {
+	var div = document.getElementById("combat-field");
+	// this is really ugly, but I'm a bad programmer
+	console.log("stuff" + i + " " + div.innerHTML);
 }
